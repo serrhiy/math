e701264edcb3cfbeb51b629ecaa7b694edd3a6be
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-04-26 21:35:18 +0300

    add types for matrix

---
+++ src/matrix.d.ts
@@ -0,0 +1,84 @@
+
+type TypedArray =
+  | Int8Array
+  | Uint8Array
+  | Uint8ClampedArray
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array
+  | BigInt64Array
+  | BigUint64Array;
+
+declare class ImmutableMatrix {
+  static identity(size: number, TypedArrayClass: TypedArray): ImmutableMatrix;
+  static fromArray(array: number[], rows: number, cols: number, TypedArrayClass: TypedArray): ImmutableMatrix;
+  static fromNestedArray(matrix: ImmutableMatrix, TypedArrayClass: TypedArray): ImmutableMatrix;
+  static fromSize(rows: number, cols: number, TypedArrayClass: TypedArray): ImmutableMatrix;
+  static fromTypedArray(typed: TypedArray, rows: number, cols: number): ImmutableMatrix;
+  static validForSum(matrix1: ImmutableMatrix, matrix2: ImmutableMatrix): boolean;
+  static fromMatrix(matrix: ImmutableMatrix): ImmutableMatrix;
+  constructor(typedMatrix: TypedArray, rows: number, cols: number, matrixConstructor: TypedArray);
+  get(row: number, col: number): number;
+  set(row: number, col: number, value: number): void;
+  get rows(): number;
+  get cols(): number;
+  get matrixConstructor(): TypedArray;
+  sum(matrix: ImmutableMatrix): ImmutableMatrix;
+  subtract(matrix: ImmutableMatrix): ImmutableMatrix;
+  mulOnNumber(x: number): ImmutableMatrix;
+  mul(matrix: ImmutableMatrix): ImmutableMatrix;
+  mulOnVector(vector: any): ImmutableMatrix;
+  pow(n: number): ImmutableMatrix;
+  compose(matrix: ImmutableMatrix): ImmutableMatrix;
+  booleanProjecion(): ImmutableMatrix;
+  toUpperTriangle(): ImmutableMatrix;
+  determinant(): number;
+  rank(): number;
+  tranpose(): ImmutableMatrix;
+  minor(row: number, col: number): number;
+  adjugate(): ImmutableMatrix;
+  inverse(): ImmutableMatrix;
+  map(fn: (element: number, index: number, matrix: ImmutableMatrix) => number, thisArg: object): ImmutableMatrix;
+}
+
+declare class MutableMatrix {
+  static identity(size: number, TypedArrayClass: TypedArray): MutableMatrix;
+  static fromArray(array: number[], rows: number, cols: number, TypedArrayClass: TypedArray): MutableMatrix;
+  static fromNestedArray(matrix: MutableMatrix, TypedArrayClass: TypedArray): MutableMatrix;
+  static fromSize(rows: number, cols: number, TypedArrayClass: TypedArray): MutableMatrix;
+  static fromTypedArray(typed: TypedArray, rows: number, cols: number): MutableMatrix;
+  static fromMatrix(matrix: MutableMatrix): MutableMatrix;
+  static validForSum(matrix1: MutableMatrix, matrix2: MutableMatrix): boolean;
+  constructor(typedMatrix: TypedArray, rows: number, cols: number, matrixConstructor: TypedArray);
+  get(row: number, col: number): number;
+  set(row: number, col: number, value: number): void;
+  get rows(): number;
+  get cols(): number;
+  get matrixConstructor(): TypedArray;
+  sum(matrix: MutableMatrix): MutableMatrix;
+  subtract(matrix: MutableMatrix): MutableMatrix;
+  mulOnNumber(x: number): MutableMatrix;
+  mul(matrix: MutableMatrix): MutableMatrix;
+  mulOnVector(vector: any): MutableMatrix;
+  pow(n: number): MutableMatrix;
+  compose(matrix: MutableMatrix): MutableMatrix;
+  booleanProjecion(): MutableMatrix;
+  toUpperTriangle(): MutableMatrix;
+  determinant(): number;
+  rank(): number;
+  tranpose(): MutableMatrix;
+  minor(row: number, col: number): number;
+  adjugate(): MutableMatrix;
+  inverse(): MutableMatrix;
+  map(fn: (element: number, index: number, matrix: MutableMatrix) => number, thisArg: object): MutableMatrix;
+}
+
+export {
+  ImmutableMatrix,
+  MutableMatrix,
+  ImmutableMatrix as immutable,
+  MutableMatrix as mutable,
+};
