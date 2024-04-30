d2fffce4d44644790ae89f43bf8b320105c25bca
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-04-30 23:29:07 +0300

    add tests for fromArray factory

--- tests/matrix/factories_test.js
+++ tests/matrix/factories_test.js
@@ -50,3 +50,56 @@ test('identity factory', async (t) => {
     assert.equal(size, cols);
   });
 });
+
+test('fromArray factory', async (t) => {
+  await t.test('empty matrix', () => {
+    const size = 0;
+    const actual = new TypedArrayClass(size);
+    const matrix = Matrix.fromArray([], size, size, TypedArrayClass);
+    const { matrix: typedArray, rows, cols } = matrix;
+    assert.deepEqual(actual, typedArray);
+    assert.equal(0, rows);
+    assert.equal(0, cols);
+  });
+
+  await t.test('1x1 matrix', () => {
+    const size = 1;
+    const array = [5];
+    const actual = new TypedArrayClass(array);
+    const matrix = Matrix.fromArray(array, size, size, TypedArrayClass);
+    const { matrix: typedArray, rows, cols } = matrix;
+    assert.deepEqual(actual, typedArray);
+    assert.equal(size, rows);
+    assert.equal(size, cols);
+  });
+
+  await t.test('3x2 matrix', () => {
+    const rows = 3;
+    const cols = 2;
+    const array = [1, 2, 3, 4, 5, 6];
+    const actual = new TypedArrayClass(array);
+    const matrix = Matrix.fromArray(array, rows, cols, TypedArrayClass);
+    const { matrix: typedArray, rows: r, cols: c } = matrix;
+    assert.deepEqual(actual, typedArray);
+    assert.equal(rows, r);
+    assert.equal(cols, c);
+  });
+
+  await t.test('5x5 matrix', () => {
+    const size = 5;
+    const array = Array.from({ length: size * size }, Math.random);
+    const actual = new TypedArrayClass(array);
+    const matrix = Matrix.fromArray(array, size, size, TypedArrayClass);
+    const { matrix: typedArray, rows, cols } = matrix;
+    assert.deepEqual(actual, typedArray);
+    assert.equal(size, rows);
+    assert.equal(size, cols);
+  });
+
+  await t.test('invalid dimension', () => {
+    const rows = 3;
+    const cols = 2;
+    const array = [1, 2, 3];
+    assert.throws(() => Matrix.fromArray(array, rows, cols));
+  });
+});
